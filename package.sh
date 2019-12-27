d04a9a6218b312dc08d4204bf955036fb7fa62d7
Nick Venenga <nick@venenga.com>
2019-12-27 16:23:13 -0500

    Add packaging step

--- package.sh
+++ package.sh
@@ -2,4 +2,15 @@
 
 which curl
 which tar
-pipenv run python -u get_cygwin_links.py
+pipenv run python -u get_cygwin_links.py | tee urls.txt
+
+mkdir -p dist
+cd dist/
+while read url; do
+  echo "Getting $url"
+  curl -O "$url"
+  file="$(basename "$url")"
+  tar --no-same-owner -xf "${file}"
+  #rm "${file}"
+done <../urls.txt
+zip -r ../dist.zip .
