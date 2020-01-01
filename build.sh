c820d0e29717964fb0590e4f51fa0c181bf0bb7f
Nick Venenga <nick@venenga.com>
2019-12-31 22:36:40 -0600

    Dynamically set output name based on versions

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -e
 
 export PYTHON=$(ls /usr/bin | grep -P "^python3\.[0-9]+$" | sort -r | head -n 1)
+export PYTHON_VERSION=$($PYTHON --version | awk '{print $2}')
 export PATH=/usr/bin:$PATH
 $PYTHON -m ensurepip
 $PYTHON -m pip install -U pip wheel
@@ -12,6 +13,7 @@ cd $(find . -maxdepth 1 -name "borgbackup*" -type d | tail -n 1 | xargs basename
 $PYTHON setup.py bdist_wheel
 
 whl=$(find . -name "*.whl" | head -n 1)
+echo "::set-output name=version::python-v${PYTHON_VERSION}-borg-v$($PYTHON setup.py --version)"
 echo "::set-output name=whl::$(cygpath -w $(readlink -f ${whl}))"
 
 exit 0
