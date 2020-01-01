0973f57fac5ec21070f067fbbcb7fedbb517c157
Nick Venenga <nick@venenga.com>
2019-12-31 22:44:05 -0600

    Add cygwin version

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 export PYTHON=$(ls /usr/bin | grep -P "^python3\.[0-9]+$" | sort -r | head -n 1)
 export PYTHON_VERSION=$($PYTHON --version | awk '{print $2}')
+export CYGWIN_VERSION=$(cygcheck -V | head -n 1 | grep -Po "[0-9.]+")
 export PATH=/usr/bin:$PATH
 $PYTHON -m ensurepip
 $PYTHON -m pip install -U pip wheel
@@ -13,7 +14,7 @@ cd $(find . -maxdepth 1 -name "borgbackup*" -type d | tail -n 1 | xargs basename
 $PYTHON setup.py bdist_wheel
 
 whl=$(find . -name "*.whl" | head -n 1)
-echo "::set-output name=version::python-v${PYTHON_VERSION}-borg-v$($PYTHON setup.py --version)"
+echo "::set-output name=version::cyg${CYGWIN_VERSION}-py${PYTHON_VERSION}-borg$($PYTHON setup.py --version)"
 echo "::set-output name=whl::$(cygpath -w $(readlink -f ${whl}))"
 
 exit 0
